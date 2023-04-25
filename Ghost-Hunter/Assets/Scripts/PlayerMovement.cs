d56a681ae238d622d18a075093192f9a987f832e
Noel Hann <nhann@csumb.edu>
2023-04-24 23:34:41 -0700

    Updated player rotation to match player movement
    Player rotation now fits with player movement. Player movement also now works in 8 directions, smoothly moving between them.

--- Ghost-Hunter/Assets/Scripts/PlayerMovement.cs
+++ Ghost-Hunter/Assets/Scripts/PlayerMovement.cs
@@ -31,6 +31,7 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
         myRigidbody.velocity = movement.normalized * speed;
+        rotateCharacter(movement.normalized);
         /*
         if (horizontalButtonPressed || verticalButtonPressed)
         {
@@ -95,10 +96,65 @@ public class PlayerMovement : MonoBehaviour
 
     private void rotateCharacter(Vector2 movement)
     {
+
+        if (movement == Vector2.zero)
+        {
+            return;
+        }
         float xDirection = movement.x;
         float yDirection = movement.y;
 
+        float myRotation = 0;
+
+        if (xDirection > 0)//if x is positive, we want to start at 0 degrees
+        {
+            myRotation = 270;
+
+            if (yDirection > 0)//if y is also positive, we want to go up a little
+            {
+                myRotation = 315;
+            }
+            else if (yDirection < 0)//if y is negative, we want to go down to 315 degrees
+            {
+                myRotation = 225;
+            }
+
+
+        }
+        else if (xDirection < 0)//if x is negative, we want to go leftish, starting at 180 degrees
+        {
+            myRotation = 90;
+
+            if (yDirection > 0)//if y is positive, we want to go left and up
+            {
+                myRotation = 45;
+            }
+            else if (yDirection < 0)
+            {
+                myRotation = 135;
+            }
+
+        }
+        else //if x is 0, we want to only use the y value
+        {
+            if (yDirection > 0)
+            {
+                myRotation = 0;
+            }
+            else
+            {
+                myRotation = 180;
+            }
+        }
         
+
+        Debug.Log($"Rotation should be {myRotation}");
+        
+        //TODO figure out how to set the rotation to the value I want
+        Vector3 newRotation = new Vector3(0, 0, myRotation);
+        myTransform.rotation = Quaternion.Euler(newRotation);
+        //myTransform.Rotate(Vector3.zero);
+        //myTransform.Rotate(new Vector3(0,0,myRotation));
     }
     
 }
