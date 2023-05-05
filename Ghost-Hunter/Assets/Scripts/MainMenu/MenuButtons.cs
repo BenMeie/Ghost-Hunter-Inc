16d95393c0a4b7c27ba2478f4a2b340e4baae3e0
Abigaëlle Panhelleux <100417910+LunaPak14@users.noreply.github.com>
2023-05-04 20:26:22 -0700

    Debug + fixes
    fixes the cursor problem and the background problem

--- Ghost-Hunter/Assets/Scripts/MainMenu/MenuButtons.cs
+++ Ghost-Hunter/Assets/Scripts/MainMenu/MenuButtons.cs
@@ -11,27 +11,20 @@ public class MenuButtons : MonoBehaviour, IPointerEnterHandler, IPointerExitHand
 {
     public GameObject button;
     private TextMesh buttonText;
-    public Texture2D cursorTexture;
-    public Texture2D cursorHover;
-    private CursorMode cursorMode = CursorMode.Auto;
-    private Vector2 hotSpot = Vector2.zero;
     public SceneFader fader;
 
     private void Start()
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
         buttonText = button.GetComponent<TextMesh>();
     }
     
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Cursor.SetCursor(cursorHover, hotSpot, cursorMode);
         buttonText.color = new Color32(52, 46, 94, 255);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        Cursor.SetCursor(cursorTexture, hotSpot, cursorMode);
         buttonText.color = new Color32(122, 109, 217, 255);
     }
 
